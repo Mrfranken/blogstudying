b6a707225734bcc69f53b181b38fbb8e1157e3b6
Mrfranken <wsj973507837@163.com>
2018-05-01 22:47:51 +0800

    learn how to use reverse to get origin url and how to use inherit in template and my css is bad

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-
+# app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)$', views.detail, name='detail')
 ]
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'people'
 ]
 
 MIDDLEWARE_CLASSES = [
